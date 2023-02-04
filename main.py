c3df500e4c1750918dbd54b8d25b9ac01278a4fd
Steven Quinn <stevenquinn@me.com>
2023-02-04 11:24:35 -0800

    Add token, separate components

--- main.py
+++ main.py
@@ -1,122 +1,73 @@
 import network
-import socket
 import time
-import json
-
+from wificonnection import WifiConnection
+from webserver import WebServer
 from servo import Servo
-from machine import Pin, PWM
-
-
-class WifiConnection:
-    
-    ssid = "Quinn Wifi"
-    password = "Conn8dsHorn"
-    max_wait = 10
-    wlan = network.WLAN(network.STA_IF)
-    
-    def __init__(self):
-        self.wlan.active(True)
-        
-    
-    def connect(self):
-        self.wlan.connect(self.ssid, self.password)
-        
-        while self.max_wait > 0:
-            if self.wlan.status() < 0 or self.wlan.status() >= 3:
-                break
-            
-            self.max_wait -= 1
-            print('waiting for connection...')
-            time.sleep(1)
-
-        if self.wlan.status() != 3:
-            raise RuntimeError('network connection failed')
-        else:
-            print('connected')
-            status = self.wlan.ifconfig()
-            print( 'ip = ' + status[0] )
-
-        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-        print('listening on', addr)
-
-        s = socket.socket()
-        s.bind(addr)
-        s.listen(1)
-        
-        return s
-        
-        
+import config
 
 
 class SmartHeater:
     
-    on_button = Servo(0)
-    temp_button = Servo(1)
-
+    on_button = Servo(config.on_pin)
+    temp_button = Servo(config.temp_pin)
+    on_button_max = 285
+    on_button_min = 0
+    temp_button_max = 300
+    temp_button_min = 0
+    button_delay = 0.5
+    
 
     def __init__(self):
-        self.wifi = WifiConnection()
-        self.s = self.wifi.connect()
+
+        self.routes = {
+            'ping': self.ping_route,
+            'on-off': self.on_off_route,
+            'temp': self.temp_route,
+        }
+
+        self.wifi = WifiConnection(config.ssid, config.password, config.api_token)
+        self.webserver = WebServer(self.wifi, self.routes)
+        self.webserver.listen()
         self.on_button.goto(0)
+        self.init_buttons()
+
+
+    #Initialize the buttons to their starting positions
+    def init_buttons(self):
         time.sleep(2)
         self.temp_button.goto(0)
         time.sleep(2)
-        self.run_server = True
         
-        
-    def is_route(self, request, route):
-        return request.find(route) == 6
     
+    # Route definitions
+    def ping_route(self, request):
+        return { "pong": True }
+
+    def on_off_route(self, request):
+        self.press_on_button()
+        return { "on": True }
+
+    def temp_route(self, request):
+        self.press_temp_button()
+        return { "temp": True }
+    
+
+    # Interacting with the pins
     def press_on_button(self):
-        self.on_button.goto(285)
-        time.sleep(0.5)
-        self.on_button.goto(0)
+        self.on_button.goto(self.on_button_max)
+        time.sleep(self.button_delay)
+        self.on_button.goto(self.on_button_min)
         
     def press_temp_button(self):
-        self.temp_button.goto(300)
-        time.sleep(0.5)
-        self.temp_button.goto(0)
+        self.temp_button.goto(self.temp_button_max)
+        time.sleep(self.button_delay)
+        self.temp_button.goto(self.temp_button_min)
     
-        
-    def listen_for_input(self):
-        
-        while self.run_server:
-            try:
-                cl, addr = self.s.accept()
-                print('client connected from', addr)
-                request = cl.recv(1024)
-                request = str(request)
-                json_data = {}
-                
-                
-                # Make sure device is alive and running
-                if self.is_route(request, '/ping'):
-                    json_data = { "pong": True }
-                    
-                if self.is_route(request, '/on-off'):
-                    self.press_on_button()
-                    json_data = { "on": True }
-                    
-                if self.is_route(request, '/temp'):
-                    self.press_temp_button()
-                    json_data = { "temp": True }
-                    
-                if self.is_route(request, '/shut-down'):
-                    self.run_server = False
-                    
 
-                cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-                cl.send(json.dumps(json_data))
-                cl.close()
-
-            except OSError as e:
-                cl.close()
-                print('connection closed')
-
+ 
     
 
 
 smart_heater = SmartHeater()
-smart_heater.listen_for_input()
 
 
